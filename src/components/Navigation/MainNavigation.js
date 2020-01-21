9ff17173866be9f1f9feb154bfb40ce448d1eda6
Richard Munoz <richosojason@msn.com>
2020-01-21 14:10:43 -0500

    added navigation header

---
+++ src/components/Navigation/MainNavigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./MainNavigation.css";
+
+const mainNavigation = props => (
+  <header className="main-navigation">
+    <div className="main-navigation__logo">
+      <h1>Pico & Placa Predictor</h1>
+    </div>
+  </header>
+);
+
+export default mainNavigation;
