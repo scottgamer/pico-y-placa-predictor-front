698222de893ebc6f9641adabaffbd52f8e632914
Richard Muñoz <richard.munoz@crifa.com>
2020-01-21 14:29:04 -0500

    added fields and refs to form

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import "./Main.css";
 
 const Main = () => {
+  const [predict, setPredict] = useState({
+    plate: "",
+    day: "",
+    hour: ""
+  });
+
+  const plateElRef = useRef(null);
+  const dayElRef = useRef(null);
+  const hourElRef = useRef(null);
+
   return (
     <div>
       <form>
@@ -11,8 +21,21 @@ const Main = () => {
           <input
             type="text"
             id="plate"
-            // ref={plateElRef}
+            ref={plateElRef}
             placeholder="Plate number"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day</label>
+          <input type="text" id="day" ref={dayElRef} placeholder="Day" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="hour">Hour</label>
+          <input
+            type="text"
+            id="hour"
+            ref={hourElRef}
+            placeholder="hour number"
           />
         </div>
       </form>
